d0d2474d99296209ccf5e054072a84df0b76687d
jgmdev <jgmdev@gmail.com>
2021-07-01 13:44:08 -0400

    Added remove_row and some micro optimization

--- listbox.lua
+++ listbox.lua
@@ -29,12 +29,12 @@ local ListBoxColumn = {}
 ---@field private expand boolean
 local ListBox = Widget:extend()
 
----Used to indicate on a widget.listbox.row row that the end
+---Indicates on a widget.listbox.row that the end
 ---of a column was reached.
 ---@type integer
 ListBox.COLEND = 1
 
----Used to indicate on a widget.listbox.row row a new line
+---Indicates on a widget.listbox.row that a new line
 ---follows while still rendering the same column.
 ---@type integer
 ListBox.NEWLINE = 2
@@ -73,6 +73,7 @@ end
 ---You can give it a table a la statusview style where you pass elements
 ---like fonts, colors, ListBox.COLEND, ListBox.NEWLINE and multiline strings.
 ---@param row widget.listbox.row
+---@param data any Associated with the row and given to on_row_click()
 function ListBox:add_row(row, data)
   table.insert(self.rows, row)
   table.insert(self.positions, self:get_col_positions(row))
@@ -80,6 +81,48 @@ function ListBox:add_row(row, data)
   if data then
     self.row_data[#self.rows] = data
   end
+
+  -- increase columns width if needed
+  if #self.columns > 0 then
+    local ridx = #self.rows
+    for col, pos in ipairs(self.positions[ridx]) do
+      if self.columns[col].expand then
+        local w = self:draw_row_range(row, pos[1], pos[2], 1, 1, true)
+        self.columns[col].width = math.max(self.columns[col].width, w)
+      end
+    end
+  end
+end
+
+-- Solution to safely remove elements from array table:
+-- found at https://stackoverflow.com/a/53038524
+local function array_remove(t, fnKeep)
+  local j, n = 1, #t;
+
+  for i=1, n do
+    if (fnKeep(t, i, j)) then
+      if (i ~= j) then
+        t[j] = t[i];
+        t[i] = nil;
+      end
+      j = j + 1;
+    else
+      t[i] = nil;
+    end
+  end
+
+  return t;
+end
+
+---Remove a given row index from the list.
+---@param ridx integer
+function ListBox:remove_row(ridx)
+  array_remove(self.rows, function(_, i, _)
+    if i == ridx then
+      return false
+    end
+    return true
+  end)
 end
 
 ---Get the starting and ending position of columns in a row table.
@@ -114,17 +157,28 @@ function ListBox:clear()
   self.rows = {}
   self.positions = {}
   self.selected_row = 0
+
+  for cidx, col in ipairs(self.columns) do
+    col.width = self:get_col_width(cidx)
+  end
 end
 
 local function lines(text)
   return (text .. "\n"):gmatch("(.-)\n")
 end
 
+---Taken from the logview and modified it a tiny bit.
+---TODO: something similar should be on lite-xl core.
 ---@param font renderer.font
 ---@param text string
 ---@param x integer
 ---@param y integer
 ---@param color renderer.color
+---@param only_calc boolean
+---@return integer resx
+---@return integer resy
+---@return integer width
+---@return integer height
 function ListBox:draw_text_multiline(font, text, x, y, color, only_calc)
   local th = font:get_height()
   local resx, resy = x, y
@@ -143,6 +197,15 @@ function ListBox:draw_text_multiline(font, text, x, y, color, only_calc)
   return resx, resy, width, height
 end
 
+---Render or calculate the size of the specified range of elements in a row.
+---@param row widget.listbox.row
+---@param start_idx integer
+---@param end_idx integer
+---@param x integer
+---@param y integer
+---@param only_calc boolean
+---@return integer width
+---@return integer height
 function ListBox:draw_row_range(row, start_idx, end_idx, x, y, only_calc)
   local font = self.font or style.font
   local color = self.foreground_color or style.text
@@ -208,6 +271,8 @@ function ListBox:get_col_width(col)
 end
 
 ---Draw the column headers of the list if available
+---@param w integer
+---@param h integer
 function ListBox:draw_header(w, h)
   local x = self.position.x + self.border.width
   local y = self.position.y + self.border.width
@@ -224,28 +289,31 @@ function ListBox:draw_header(w, h)
   end
 end
 
----@param self widget.listbox
+---Draw or calculate the size of the given row position.
 ---@param row integer
+---@param x integer
+---@param y integer
+---@param only_calc boolean
+---@return integer width
+---@return integer height
 function ListBox:draw_row(row, x, y, only_calc)
   local w, h = 0, 0
 
-  if not only_calc then
-    if self:mouse_on_top(self.mouse.x, self.mouse.y) then
-      w, h = self:draw_row(row, x, y, true)
-      w = self.largest_row > 0 and self.largest_row or w
-      local mouse = self.mouse
-      if
-        mouse.x >= x
-        and
-        mouse.x <= x + w
-        and
-        mouse.y >= y
-        and
-        mouse.y <= y + h
-      then
-        renderer.draw_rect(x, y, w, h, style.selection)
-        self.selected_row = row
-      end
+  if not only_calc and self.rows[row].w then
+    w, h = self.rows[row].w, self.rows[row].h
+    w = self.largest_row > 0 and self.largest_row or w
+    local mouse = self.mouse
+    if
+      mouse.x >= x
+      and
+      mouse.x <= x + w
+      and
+      mouse.y >= y
+      and
+      mouse.y <= y + h
+    then
+      renderer.draw_rect(x, y, w, h, style.selection)
+      self.selected_row = row
     end
     w, h = 0, 0
   end
@@ -283,8 +351,19 @@ function ListBox:draw_row(row, x, y, only_calc)
     w = cw + style.padding.x
   end
 
+  -- Add padding on top and bottom
+  h = h + style.padding.y
+
+  -- store the dimensions for inexpensive subsequent hover calculation
+  self.rows[row].w = w
+  self.rows[row].h = h
+
+  -- TODO: performance improvement, render only the visible rows on the view?
+  self.rows[row].x = x
+  self.rows[row].y = y - (style.padding.y / 2)
+
   -- return height with padding on top and bottom
-  return w, h + style.padding.y
+  return w, h
 end
 
 ---
@@ -313,16 +392,21 @@ end
 
 ---You can overwrite this to listen to item clicks
 ---@param idx integer
----@param data any
+---@param data any Data associated with the row
 function ListBox:on_row_click(idx, data) end
 
+local last_scale_update = 0
 function ListBox:update()
   if not ListBox.super.update(self) then return end
 
-  if #self.columns > 0 then
-    for col, column in ipairs(self.columns) do
-      column.width = self:get_col_width(col)
+  -- only calculate columns width on scale change since this can be expensive
+  if last_scale_update ~= SCALE then
+    if #self.columns > 0 then
+      for col, column in ipairs(self.columns) do
+        column.width = self:get_col_width(col)
+      end
     end
+    last_scale_update = SCALE
   end
 end
 
