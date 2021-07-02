c460685f8f306c3b2584b3800f57a68f75686afb
jgmdev <jgmdev@gmail.com>
2021-07-02 17:40:13 -0400

    Fixed listbox example

--- examples/listbox.lua
+++ examples/listbox.lua
@@ -2,10 +2,8 @@
 -- Basic listbox example.
 --
 
-local core = require "core"
 local style = require "core.style"
 local Widget = require "widget"
-local Button = require "widget.button"
 local ListBox = require "widget.listbox"
 
 ---@type widget
@@ -20,33 +18,29 @@ widget:centered()
 
 ---@type widget.listbox
 local listbox = ListBox(widget)
-
+listbox.size.y = widget.size.y - widget.border.width*2
+listbox:centered()
 
 listbox:add_row({
-  style.icon_font, style.syntax.string, "!", style.font, style.text, " Error",
+  style.icon_font, style.syntax.string, "!", style.font, style.text, " Error, ",
   ListBox.COLEND,
   "A message."
 })
+for i=1, 10000 do
+  listbox:add_row({
+    tostring(i) .. ". Good ",
+    ListBox.COLEND,
+    "Hi!."
+  })
+end
 listbox:add_row({
-  "Good",
-  ListBox.COLEND,
-  "Hi!."
-})
-listbox:add_row({
-  "More",
+  "More ",
   ListBox.COLEND,
   "Final message."
 })
 
 listbox.on_row_click = function(self, idx, data)
   system.show_fatal_error("Clicked a row", idx)
-end
-
-listbox.update = function(self)
-  ListBox.super.update(self)
-  self:set_position(0, 0)
-  self:set_size(self.parent:get_width() - 2, self.parent:get_height() - 2)
-  self.largest_row = self.parent:get_width() - 2
 end
 
 widget:show()
