56381539986e3418f517805d612f7df958d48dcc
jgmdev <jgmdev@gmail.com>
2021-07-04 23:04:54 -0400

    [label] added support to show styled text.

--- label.lua
+++ label.lua
@@ -12,30 +12,46 @@ local Label = Widget:extend()
 
 function Label:new(parent, label)
   Label.super.new(self, parent)
+  self.clickable = false
   self:set_label(label or "")
 end
 
+---@param text string|widget.styledtext
 function Label:set_label(text)
   Label.super.set_label(self, text)
 
-  self.size.x = self.font:get_width(self.label)
-  self.size.y = self.font:get_height()
+  if type(text) == "table" then
+    self.size.x, self.size.y = self:draw_styled_text(text, 0, 0, true)
+  else
+    self.size.x = self.font:get_width(self.label)
+    self.size.y = self.font:get_height()
+  end
 end
 
 function Label:update()
-  Label.super.update(self)
+  if not Label.super.update(self) then return end
   -- update the size
   self:set_label(self.label)
 end
 
 function Label:draw()
-  renderer.draw_text(
-    self.font,
-    self.label,
-    self.position.x,
-    self.position.y,
-    self.foreground_color or style.text
-  )
+  if
+    not self.visible or (self.parent and not self.parent.visible)
+  then
+    return
+  end
+
+  if type(self.label) == "table" then
+    self:draw_styled_text(self.label, self.position.x, self.position.y)
+  else
+    renderer.draw_text(
+      self.font,
+      self.label,
+      self.position.x,
+      self.position.y,
+      self.foreground_color or style.text
+    )
+  end
 end
 
 
