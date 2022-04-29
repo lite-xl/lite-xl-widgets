8b6c4ab55668035265ef5587b069931a029e8f7a
jgmdev <jgmdev@gmail.com>
2022-04-29 16:32:33 -0400

    label: optional support for border

--- label.lua
+++ label.lua
@@ -13,6 +13,7 @@ local Label = Widget:extend()
 function Label:new(parent, label)
   Label.super.new(self, parent)
   self.clickable = false
+  self.border.width = 0
   self:set_label(label or "")
 end
 
@@ -25,6 +26,11 @@ function Label:set_label(text)
   else
     self.size.x = self.font:get_width(self.label)
     self.size.y = self.font:get_height()
+  end
+
+  if self.border.width > 0 then
+    self.size.x = self.size.x + style.padding.x
+    self.size.y = self.size.y + style.padding.y
   end
 end
 
@@ -40,14 +46,19 @@ end
 function Label:draw()
   if not self:is_visible() then return false end
 
+  self:draw_border()
+
+  local px = self.border.width > 0 and (style.padding.x / 2) or 0
+  local py = self.border.width > 0 and (style.padding.y / 2) or 0
+
   if type(self.label) == "table" then
     self:draw_styled_text(self.label, self.position.x, self.position.y)
   else
     renderer.draw_text(
       self.font,
       self.label,
-      self.position.x,
-      self.position.y,
+      self.position.x + px,
+      self.position.y + py,
       self.foreground_color or style.text
     )
   end
