aefaadf3d6bb24cfec70f747b24ca8b7d929eeca
jgmdev <jgmdev@gmail.com>
2022-05-16 16:50:20 -0400

    line: added set_width() and improve size calculation

--- line.lua
+++ line.lua
@@ -9,6 +9,7 @@ local Widget = require "widget"
 
 ---@class widget.line : widget
 ---@field public padding integer
+---@field private custom_width number
 local Line = Widget:extend()
 
 ---Constructor
@@ -18,6 +19,8 @@ local Line = Widget:extend()
 function Line:new(parent, thickness, padding)
   Line.super.new(self, parent)
   self.size.y = thickness or 2
+  self.custom_width = nil
+  self.border.width = 0
   self.padding = padding or (style.padding.x / 2)
 end
 
@@ -27,15 +30,24 @@ function Line:set_thickness(thickness)
   self.size.y  = thickness or 2
 end
 
+---Set a custom width for the line
+---@param width number
+function Line:set_width(width)
+  self.custom_width = width
+  self.size.x = width
+end
+
 function Line:draw()
   if not self:is_visible() then return false end
 
-  self.size.x = self.parent.size.x
+  if not self.custom_width then
+    self.size.x = self.parent.size.x - (self.padding * 2)
+  end
 
   renderer.draw_rect(
     self.position.x + self.padding,
     self.position.y,
-    self.size.x - (self.padding * 2),
+    self.size.x,
     self.size.y,
     self.foreground_color or style.caret
   )
